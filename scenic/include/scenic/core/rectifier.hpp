6b9509f8b5aa1e19c15351a2108d0dc010386f00
jason <jasonah@seas.upenn.edu>
2026-01-06 10:54:07 -0500

    added rectifier from topographer

---
+++ scenic/include/scenic/core/rectifier.hpp
@@ -0,0 +1,66 @@
+/*
+* Jason Hughes
+* June 2025
+*
+* rectifier object
+*/
+
+#pragma once
+
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <yaml-cpp/yaml.h>
+
+namespace Scenic
+{
+
+class Rectifier
+{
+    public:
+        Rectifier() = default;
+        Rectifier(std::string path);
+
+        static Rectifier Load(std::string path);
+
+        void setIntrinsics(const cv::Mat& i);
+        void setDistortion(const cv::Mat& d);
+        void setTranslation(const cv::Mat& t);
+        void setTransformBodyCam(const Eigen::Vector3d& t);
+
+        void setResolution(const int& w, const int& h);
+        void setWidth(const int& w);
+        void setHeight(const int& h);
+
+        void rectifyMonoImage(const cv::Mat& input, cv::Mat& output) const;
+        void calculateOutputIntrinsics();
+        
+        template<typename T>
+        T getWidth() const;
+        template<typename T>
+        T getHeight() const;
+
+        template<typename T>
+        T getIntrinsics() const;
+        template<typename T>
+        T getOutputIntrinsics() const;
+
+        cv::Size getResolution() const;
+        cv::Size getOutputResolution() const;
+        Eigen::Isometry3d getTransformBodyCam() const;
+
+    private:
+        cv::Mat intrinsics_;
+        cv::Mat distortion_;
+        cv::Mat output_intrinsics_;
+
+        Eigen::Isometry3d T_body_cam_;
+
+        size_t height_;
+        size_t width_;
+        cv::Size resolution_;
+        cv::Size output_res_;
+};
+}
---
+++ scenic/src/rectifier.cpp
@@ -0,0 +1,240 @@
+/*
+* Jason Hughes
+* June 2025
+*
+*/
+#include <cassert>
+#include "scenic/core/rectifier.hpp"
+
+using namespace Scenic;
+
+Rectifier::Rectifier(std::string path)
+{
+    try
+    {
+        YAML::Node config = YAML::LoadFile(path);
+
+        auto cam = config["cam0"];
+        auto intrinsics = cam["intrinsics"];
+        intrinsics_ = (cv::Mat_<double>(3,3) << intrinsics[0].as<double>(), 0, intrinsics[2].as<double>(),
+                                                0, intrinsics[1].as<double>(), intrinsics[3].as<double>(),
+                                                0, 0, 1);
+
+        auto distortion = cam["distortion_coeffs"];
+        distortion_ = cv::Mat::zeros(4,1,CV_64F);
+        for (size_t ind=0; ind<4; ind++)
+        {
+            distortion_.at<double>(ind, 0) = cam["distortion_coeffs"][ind].as<double>();
+        }
+        
+
+        width_ = cam["resolution"][0].as<int>();
+        height_ = cam["resolution"][1].as<int>();
+        resolution_ = cv::Size(width_, height_);
+        output_res_ = cv::Size(width_*0.5, height_*0.5);
+        calculateOutputIntrinsics();
+        
+        auto t_body_cam = cam["t_body_cam"];
+        Eigen::Vector3d t(t_body_cam[0].as<double>(), t_body_cam[1].as<double>(), t_body_cam[2].as<double>());
+        setTransformBodyCam(t);
+        
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "[TOPOGRAPHER] Error loading calibration file at " << path << ", with error: " << e.what() << std::endl;
+    }
+}
+
+Rectifier Rectifier::Load(std::string path)
+{
+    Rectifier rect;
+    try
+    {
+        YAML::Node config = YAML::LoadFile(path);
+
+        auto cam = config["cam0"];
+        auto intrinsics = cam["intrinsics"];
+        cv::Mat intrin = (cv::Mat_<double>(3,3) << intrinsics[0].as<double>(), 0, intrinsics[2].as<double>(),
+                                                0, intrinsics[1].as<double>(), intrinsics[3].as<double>(),
+                                                0, 0, 1);
+        rect.setIntrinsics(intrin);
+
+        auto distortion = cam["distortion_coeffs"];
+        cv::Mat dist = cv::Mat::zeros(4,1,CV_64F);
+        for (size_t ind=0; ind<4; ind++)
+        {
+            dist.at<double>(ind, 0) = cam["distortion_coeffs"][ind].as<double>();
+        }
+        
+        rect.setDistortion(dist);
+
+        int width = cam["resolution"][0].as<int>();
+        int height = cam["resolution"][1].as<int>();
+        auto t_body_cam = cam["T_body_cam"];
+        Eigen::Vector3d t(t_body_cam[0].as<double>(), t_body_cam[1].as<double>(), t_body_cam[2].as<double>());
+
+        rect.setTransformBodyCam(t);
+        rect.setResolution(width, height);
+        rect.calculateOutputIntrinsics();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "[SCENIC] Error loading calibration file at " << path << ", with error: " << e.what() << std::endl;
+    }
+
+    return rect;
+}
+
+void Rectifier::rectifyMonoImage(const cv::Mat& input, cv::Mat& output) const
+{
+    static cv::Mat map1, map2;
+    static bool maps_initialized = false;
+    
+    if (!maps_initialized)
+    {
+        cv::initUndistortRectifyMap(intrinsics_, distortion_, cv::Mat(),
+                                    intrinsics_, cv::Size(width_, height_),
+                                    CV_16SC2, map1, map2);
+        maps_initialized = true;
+    }
+    
+    cv::remap(input, output, map1, map2, cv::INTER_LINEAR);
+}
+
+void Rectifier::setIntrinsics(const cv::Mat& i)
+{
+    intrinsics_ = i;
+}
+
+void Rectifier::setDistortion(const cv::Mat& d)
+{
+    distortion_ = d;
+}
+
+void Rectifier::setResolution(const int& w, const int& h)
+{
+    width_ = w;
+    height_ = h;
+    resolution_ = cv::Size(w, h);
+    output_res_ = cv::Size(w/2, h/2);
+}
+
+void Rectifier::setWidth(const int& w)
+{
+    width_ = w;
+}
+
+void Rectifier::setHeight(const int& h)
+{
+    height_ = h;
+}
+
+void Rectifier::calculateOutputIntrinsics()
+{
+    // keep the input size and the output size the same as the resolution for now.
+    output_intrinsics_ = cv::getOptimalNewCameraMatrix(intrinsics_, distortion_, resolution_, 0, output_res_);
+}
+
+void Rectifier::setTransformBodyCam(const Eigen::Vector3d& t)
+{
+    T_body_cam_ = Eigen::Isometry3d::Identity();
+    T_body_cam_.translation() = t;
+}
+
+Eigen::Isometry3d Rectifier::getTransformBodyCam() const
+{
+    return T_body_cam_;
+}
+
+template<typename T>
+T Rectifier::getHeight() const
+{
+    if constexpr (std::is_same_v<T, int>)
+    {
+        return static_cast<int>(height_);
+    }
+    else
+    {
+        return height_;
+    }
+}
+
+template<typename T>
+T Rectifier::getWidth() const
+{
+    if constexpr (std::is_same_v<T, int>)
+    {
+        return static_cast<int>(width_);
+    }
+    else
+    {
+        return width_;
+    }
+}  
+
+template<typename T>
+T Rectifier::getIntrinsics() const
+{
+    if constexpr (std::is_same_v<T, Eigen::Matrix3d>)
+    {
+        Eigen::Matrix3d k;
+        for (int i = 0; i < 3; ++i) 
+        {
+            for (int j = 0; j < 3; ++j) 
+            {
+                k(i, j) = intrinsics_.at<double>(i, j);
+            }
+        }
+        return k;
+    }
+    else
+    {
+        return intrinsics_;
+    }
+}
+
+template<typename T>
+T Rectifier::getOutputIntrinsics() const
+{
+    if constexpr (std::is_same_v<T, Eigen::Matrix3d>)
+    {
+        Eigen::Matrix3d k;
+        for (int i = 0; i < 3; ++i) 
+        {
+            for (int j = 0; j < 3; ++j) 
+            {
+                k(i, j) = output_intrinsics_.at<double>(i, j);
+            }
+        }
+        return k;
+    }
+    else
+    {
+        return output_intrinsics_;
+    }
+}
+
+cv::Size Rectifier::getResolution() const
+{
+    cv::Size res(width_, height_);
+
+    return res;
+}
+
+cv::Size Rectifier::getOutputResolution() const
+{
+    return output_res_;
+}
+
+
+template int Rectifier::getHeight<int>() const;
+template size_t Rectifier::getHeight<size_t>() const;
+
+template int Rectifier::getWidth<int>() const;
+template size_t Rectifier::getWidth<size_t>() const;
+
+template cv::Mat Rectifier::getIntrinsics<cv::Mat>() const;
+template Eigen::Matrix3d Rectifier::getIntrinsics<Eigen::Matrix3d>() const;
+
+template cv::Mat Rectifier::getOutputIntrinsics<cv::Mat>() const;
+template Eigen::Matrix3d Rectifier::getOutputIntrinsics<Eigen::Matrix3d>() const;
